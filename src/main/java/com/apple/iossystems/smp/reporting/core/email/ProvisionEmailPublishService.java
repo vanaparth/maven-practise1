2d72d05a0fb37c290caa637a434b893978e1777f
Tochukwu Iwuchukwu <tiwuchukwu@apple.com>
2015-03-06 22:08:58 +0000

    <rdar://problem/20063774> No welcome email when pass becomes active after yellow provisioning
    git-svn-id: https://istsvn.corp.apple.com:1080/SVNROOT/STOCKHOLM/branches/1501A@20782 b4b91761-3d01-0010-a18b-a3cb8716a3e3
    
    Former-commit-id: 4648fbca4d313ee7975fed726a776edc4ecb5f5c

--- src/main/java/com/apple/iossystems/smp/reporting/core/email/ProvisionEmailPublishService.java
+++ src/main/java/com/apple/iossystems/smp/reporting/core/email/ProvisionEmailPublishService.java
@@ -1,17 +1,44 @@
 package com.apple.iossystems.smp.reporting.core.email;
 
+import com.apple.cds.keystone.spring.AppContext;
+import com.apple.iossystems.smp.domain.DSIDInfo;
+import com.apple.iossystems.smp.domain.ProvisionCount;
 import com.apple.iossystems.smp.domain.jsonAdapter.GsonBuilderFactory;
+import com.apple.iossystems.smp.service.StoreManagementService;
+import org.apache.log4j.Logger;
 
 /**
  * @author Toch
  */
 public class ProvisionEmailPublishService
 {
+    private static final Logger LOGGER = Logger.getLogger(ProvisionEmailPublishService.class);
+
     private ProvisionEmailPublishService()
     {
     }
 
-    public static void publishEvent(String conversationId, boolean firstProvision)
+    public static void processProvisionEvent(String conversationId, String dsid)
+    {
+        StoreManagementService storeManagementService = AppContext.getApplicationContext().getBean(StoreManagementService.class);
+
+        DSIDInfo dsidInfo = null;
+
+        try
+        {
+            dsidInfo = storeManagementService.getDSIDInfo(dsid);
+        }
+        catch (Exception e)
+        {
+            LOGGER.error(e);
+        }
+
+        publishEvent(conversationId, ((dsidInfo != null) && (ProvisionCount.ZERO.equals(dsidInfo.getProvisionCount()))));
+
+        storeManagementService.updateProvisionCount(dsid);
+    }
+
+    private static void publishEvent(String conversationId, boolean firstProvision)
     {
         if (firstProvision)
         {

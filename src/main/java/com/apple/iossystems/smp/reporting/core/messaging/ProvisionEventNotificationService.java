a3da734b6e57439ccfa91f0147ebf9e797f65790
Tochukwu Iwuchukwu <tiwuchukwu@apple.com>
2015-06-29 22:54:40 +0000

    <rdar://problem/21525985> SMPReporting: Code Refactor
    git-svn-id: https://istsvn.corp.apple.com:1080/SVNROOT/STOCKHOLM/trunk@24116 b4b91761-3d01-0010-a18b-a3cb8716a3e3
    
    Former-commit-id: f2209e62773c7caeb5a339a651aec28d35265de7

--- src/main/java/com/apple/iossystems/smp/reporting/core/messaging/ProvisionEventNotificationService.java
+++ src/main/java/com/apple/iossystems/smp/reporting/core/messaging/ProvisionEventNotificationService.java
@@ -1,9 +1,11 @@
-package com.apple.iossystems.smp.reporting.core.email;
+package com.apple.iossystems.smp.reporting.core.messaging;
 
 import com.apple.cds.keystone.spring.AppContext;
 import com.apple.iossystems.smp.domain.DSIDInfo;
 import com.apple.iossystems.smp.domain.ProvisionCount;
 import com.apple.iossystems.smp.domain.jsonAdapter.GsonBuilderFactory;
+import com.apple.iossystems.smp.reporting.core.email.EmailService;
+import com.apple.iossystems.smp.reporting.core.email.ProvisionCardEvent;
 import com.apple.iossystems.smp.reporting.core.persistence.SMPEventCache;
 import com.apple.iossystems.smp.service.StoreManagementService;
 import org.apache.log4j.Logger;
@@ -11,21 +13,21 @@ import org.apache.log4j.Logger;
 /**
  * @author Toch
  */
-public class ProvisionEmailPublishService
+public class ProvisionEventNotificationService
 {
-    private static final Logger LOGGER = Logger.getLogger(ProvisionEmailPublishService.class);
+    private static final Logger LOGGER = Logger.getLogger(ProvisionEventNotificationService.class);
 
     private EmailService emailService = EmailService.getInstance();
 
     private static final SMPEventCache SMP_EVENT_CACHE = SMPEventCache.getInstance();
 
-    private static final ProvisionEmailPublishService INSTANCE = new ProvisionEmailPublishService();
+    private static final ProvisionEventNotificationService INSTANCE = new ProvisionEventNotificationService();
 
-    private ProvisionEmailPublishService()
+    private ProvisionEventNotificationService()
     {
     }
 
-    public static ProvisionEmailPublishService getInstance()
+    public static ProvisionEventNotificationService getInstance()
     {
         return INSTANCE;
     }

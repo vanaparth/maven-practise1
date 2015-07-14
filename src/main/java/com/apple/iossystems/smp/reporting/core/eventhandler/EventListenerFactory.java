7cc60efa26cba3bf6dbf01b86f71b0412e1de9fe
Tochukwu Iwuchukwu <tiwuchukwu@apple.com>
2015-07-14 22:03:34 +0000

    <rdar://problem/21822730> SMPReporting: Code Refactor
    git-svn-id: https://istsvn.corp.apple.com:1080/SVNROOT/STOCKHOLM/trunk@24293 b4b91761-3d01-0010-a18b-a3cb8716a3e3
    
    Former-commit-id: a1b80ab143b6f8eb36c54187f2a7d95bf5809db7

--- src/main/java/com/apple/iossystems/smp/reporting/core/eventhandler/EventListenerFactory.java
+++ src/main/java/com/apple/iossystems/smp/reporting/core/eventhandler/EventListenerFactory.java
@@ -1,16 +1,10 @@
 package com.apple.iossystems.smp.reporting.core.eventhandler;
 
-import com.apple.cds.keystone.config.PropertyManager;
-import org.apache.commons.lang.StringUtils;
-import org.apache.log4j.Logger;
-
 /**
  * @author Toch
  */
 public class EventListenerFactory
 {
-    private static final Logger LOGGER = Logger.getLogger(EventListenerFactory.class);
-
     private EventListenerFactory()
     {
     }
@@ -20,42 +14,19 @@ public class EventListenerFactory
         return new EventListenerFactory();
     }
 
-    private EventListener getEventListener(String propertyKey, String defaultClassName)
-    {
-        EventListener eventListener = null;
-
-        try
-        {
-            Class eventListenerClass = Class.forName(getFullClassName(PropertyManager.getInstance().valueForKeyWithDefault(propertyKey, defaultClassName)));
-
-            eventListener = (EventListener) eventListenerClass.newInstance();
-        }
-        catch (Exception e)
-        {
-            LOGGER.error(e);
-        }
-
-        return eventListener;
-    }
-
-    private String getFullClassName(String className)
-    {
-        return (StringUtils.isNotBlank(className) && (!className.contains("."))) ? "com.apple.iossystems.smp.reporting.core.eventhandler." + className : className;
-    }
-
     public EventListener getEmailEventListener()
     {
-        return getEventListener("smp.reporting.emailEventListener", "EmailEventListener");
+        return new EmailEventListener();
     }
 
     public EventListener getSMPPublishEventListener()
     {
-        return getEventListener("smp.reporting.smpPublishEventListener", "SMPPublishEventListener");
+        return new SMPPublishEventListener();
     }
 
     public EventListener getSMPConsumeEventListener()
     {
-        return getEventListener("smp.reporting.smpConsumeEventListener", "SMPConsumeEventListener");
+        return new SMPConsumeEventListener();
     }
 
     public EventListener getSMPKistaEventListener()

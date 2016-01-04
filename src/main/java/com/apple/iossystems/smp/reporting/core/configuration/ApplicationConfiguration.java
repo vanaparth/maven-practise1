eabb61a960fe111c6f2bd8753ed49393f889acf2
Yousuf Vaid <yvaid@apple.com>
2016-01-04 23:04:30 +0000

    <rdar://problem/23501282> Default Welcome Email should not be US-en --Removed pieces not required for email from Toch's original check-in in trunk.
    git-svn-id: https://istsvn.corp.apple.com:1080/SVNROOT/STOCKHOLM/branches/1B08_01@27828 b4b91761-3d01-0010-a18b-a3cb8716a3e3
    
    Former-commit-id: a65839a720c725919a28ad19c78b092920ea0406

--- src/main/java/com/apple/iossystems/smp/reporting/core/configuration/ApplicationConfiguration.java
+++ src/main/java/com/apple/iossystems/smp/reporting/core/configuration/ApplicationConfiguration.java
@@ -1,7 +1,7 @@
 package com.apple.iossystems.smp.reporting.core.configuration;
 
 import com.apple.cds.keystone.config.PropertyManager;
-import com.apple.maus.util.StringUtils;
+import org.apache.commons.lang.StringUtils;
 
 import java.util.*;
 
@@ -40,7 +40,7 @@ public class ApplicationConfiguration
     static final boolean PROVISION_EMAIL_ENABLED = PROPERTY_MANAGER.getBooleanValueForKeyWithDefault("smp.reporting.email.provision", true);
     static final boolean SUSPEND_EMAIL_ENABLED = PROPERTY_MANAGER.getBooleanValueForKeyWithDefault("smp.reporting.email.suspend", true);
     static final boolean UNLINK_EMAIL_ENABLED = PROPERTY_MANAGER.getBooleanValueForKeyWithDefault("smp.reporting.email.unlink", true);
-    static final boolean DEFAULT_EMAIL_LOCALE_ENABLED = PROPERTY_MANAGER.getBooleanValueForKeyWithDefault("smp.reporting.email.locale", true);
+    static final boolean DEFAULT_EMAIL_LOCALE_ENABLED = PROPERTY_MANAGER.getBooleanValueForKeyWithDefault("smp.reporting.email.default.locale", true);
 
     static final String FMIP_CERTIFICATE = PROPERTY_MANAGER.valueForKeyWithDefault("com.apple.iossystems.internal.fmip.app.cert", "0");
     static final String FMIP_REMOTE_CERTIFICATE = PROPERTY_MANAGER.valueForKeyWithDefault("com.apple.iossystems.internal.fmip.setup.cert", "0");
